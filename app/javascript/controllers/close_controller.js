401f1cb355fbf733d20176f2a425595d99a50b71
rasyidannas <rasshit.dsgn@gmail.com>
2024-12-10 07:00:18 +0700

    4/6. alert and notice message with stimulus.mp4

--- app/javascript/controllers/close_controller.js
+++ app/javascript/controllers/close_controller.js
@@ -2,11 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="close"
 export default class extends Controller {
-  // connect() {
-  //   console.log("hello, stimulus");
-  // }
+  static targets = ["alert"];
+  static values = { timeout: Number };
+  static classes = ["hidden"];
+
+  connect() {
+    setTimeout(() => this.hide(), this.timeoutValue);
+  }
 
   hide() {
-    console.log("hide");
+    this.alertTarget.classList.add(this.hiddenClass);
   }
 }
