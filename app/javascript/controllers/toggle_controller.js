add69fbc514e782fa723f44a31dedbbec5b1a71f
rasyidannas <rasshit.dsgn@gmail.com>
2025-01-13 16:23:48 +0700

    5/11. Toggle the Sidebar.mp4

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,13 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["profileCard"];
+  static targets = ["profileCard", "sidebar"];
 
   conncts() {
-    console.log("toggle_controller.js connected")
+    console.log("toggle_controller.js connected");
   }
 
   toggleProfileCard() {
     this.profileCardTarget.classList.toggle("hidden");
+  }
+
+  toggleSidebar() {
+    this.sidebarTarget.classList.toggle("hidden");
   }
 }
